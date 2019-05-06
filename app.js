4ad68461971d9c6724ced4fc0fe0d364e7f01189
Churchill Exotics <churchillexotics@iMac.local>
2019-05-06 14:10:39 -0400

    add a new file

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-document.documentElement.style .setProperty('--primary', '#ADCBCD');
+document.documentElement.style .setProperty('--primary', '#a9eec2');
 document.documentElement.style .setProperty('--secondary', '#ddd');
 document.documentElement.style .setProperty('--tertiary', '#FAFAFA');
 document.documentElement.style .setProperty('--quaternary', '#FAFAFA');
 document.documentElement.style .setProperty('--border', '#d8d8d8');
 document.documentElement.style .setProperty('--page-max-width', '1024px');
-document.documentElement.style .setProperty('--navbar-background', 'black');
-document.documentElement.style .setProperty('--navbar-text-color', 'white');
-document.documentElement.style .setProperty('--header-background', 'black');
+document.documentElement.style .setProperty('--navbar-background', '#a9eec2');
+document.documentElement.style .setProperty('--navbar-text-color', '#5c7658');
+document.documentElement.style .setProperty('--header-background', '#7fa99b');
 document.documentElement.style .setProperty('--header-text-color', 'white');
-document.documentElement.style .setProperty('--footer-background', 'black');
-document.documentElement.style .setProperty('--footer-text-color', 'white');
+document.documentElement.style .setProperty('--footer-background', '#a9eec2');
+document.documentElement.style .setProperty('--footer-text-color', '#5c7658');
 document.documentElement.style .setProperty('--h1-color', 'black');
 document.documentElement.style .setProperty('--h2-color', '#000');
 document.documentElement.style .setProperty('--h3-color', '#262626');
@@ -42,19 +42,25 @@ const footer_data = [
 ];
 function footer_template(foot) {
     return `
-	<div class="footer">
-     <div class="grid set-cols-1">
-       <div class="column max-vertical-between">
-          <i class="fas fa-star center "></i>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-		<ul class="menu-list">
-		  <li><a href="/">link 1</a></li>
-		  <li><a href="/">link 2</a></li>
-		  <li><a href="/">link 3</a></li>
-		</ul>
-       </div>
-     </div>
-	</div>
+	
+    <div class="grid set-cols-1">
+        <div class="column start-vertical-between">
+            <h4>The #1 Insurance Site</h4>
+            <h4>Copyright 1995 - 2019. Progressive Casualty Insurance Company. All Rights Reserved.</h4>
+            <p>We offer insurance by phone, online and through independent agents. Prices vary based on how you
+            buy.</p>
+            <p>Mobile rates are for policies purchased directly from Progressive.</p>
+             <p>*National average annual car insurance savings by new customers surveyed who saved with
+            Progressive in 2018.</p>
+            <p>Progressive Home Advantage® policies are placed through Progressive Advantage Agency, Inc. with
+            affiliated and third-party insurers who are solely responsible for claims, and pay PAA
+            commission for policies sold. Prices, coverages, privacy policies, and PAA's commission vary
+            among these insurers. How you buy (phone, online, mobile, or independent agent/broker)
+            determines which insurers are available to you. Click here for a list of the insurers or contact
+            us for more information about PAA's commission. Discounts not available in all states and
+            situations.</p>
+        </div>
+    </div>
   `;
 }
 
